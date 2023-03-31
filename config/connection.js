743217935aa16010198921cc472fd4d0ec75cd27
Tochio Herrera <tochio12910@gmail.com>
2023-03-31 19:05:27 -0400

    added the connections

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose');
+const { connect, connection } = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmediaDb', {
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialMediaDb';
+
+connect(connectionString, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
